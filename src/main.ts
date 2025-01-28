24717d550bec7228cd7106e291050b07b3f9ae23
Xurshid Ismoilov <khurshidi2827@gmail.com>
2025-01-29 03:10:14 +0500

    >

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ import * as fs from "fs"
 
 async function bootstrap() {
 
-  const app = await NestFactory.create(AppModule,{cors : true});
+  const app = await NestFactory.create(AppModule,{cors : false});
 
   app.use(function (req, res, next) {
     //Enabling CORS
