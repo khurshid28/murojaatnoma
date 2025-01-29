12b88759690494bca6e0a5c908afe3e8744d29db
Xurshid Ismoilov <khurshidi2827@gmail.com>
2025-01-29 14:06:56 +0500

    >

--- src/main.ts
+++ src/main.ts
@@ -4,40 +4,43 @@ import { AppModule } from './app.module';
 import * as bodyParser from 'body-parser';
 import * as multer from 'multer';
 
-import * as morgan from "morgan"
+import * as morgan from 'morgan';
 
-import * as fs from "fs"
+import * as fs from 'fs';
 
 async function bootstrap() {
-
-  const app = await NestFactory.create(AppModule,{
-    cors : {
-      origin : true,
-      
-
-    }
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: true,
+    },
   });
 
   app.use(function (req, res, next) {
     //Enabling CORS
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
-    // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-      next();
-    });
- 
-  app.use(morgan(':method :url :status :res[content-length] - :response-time ms')
-  )
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+      'Access-Control-Allow-Methods',
+      'GET,HEAD,OPTIONS,POST,PUT,DELETE',
+    );
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization',
+    );
+    next();
+  });
 
-  app.use(bodyParser.urlencoded({ extended: true, limit: "10mb" }))
-  app.use(bodyParser.text({ type: 'text/html' }))
-  app.use(bodyParser.json({
-    limit: "10mb"
-  }));
+  app.use(
+    morgan(':method :url :status :res[content-length] - :response-time ms'),
+  );
 
+  app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
+  app.use(bodyParser.text({ type: 'text/html' }));
+  app.use(
+    bodyParser.json({
+      limit: '10mb',
+    }),
+  );
 
   await app.listen(process.env.PORT || 3000);
-
-
 }
 bootstrap();
