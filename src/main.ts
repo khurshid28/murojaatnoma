9cdc290ec15597d5ddacf05888cd3c53807e3b98
Xurshid Ismoilov <khurshidi2827@gmail.com>
2025-01-29 14:29:52 +0500

    >

--- src/main.ts
+++ src/main.ts
@@ -15,19 +15,19 @@ async function bootstrap() {
     },
   });
 
-  app.use(function (req, res, next) {
-    //Enabling CORS
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-      'Access-Control-Allow-Methods',
-      'GET,HEAD,OPTIONS,POST,PUT,DELETE',
-    );
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization',
-    );
-    next();
-  });
+  // app.use(function (req, res, next) {
+  //   //Enabling CORS
+  //   res.header('Access-Control-Allow-Origin', '*');
+  //   res.header(
+  //     'Access-Control-Allow-Methods',
+  //     'GET,HEAD,OPTIONS,POST,PUT,DELETE',
+  //   );
+  //   res.header(
+  //     'Access-Control-Allow-Headers',
+  //     'Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization',
+  //   );
+  //   next();
+  // });
 
   app.use(
     morgan(':method :url :status :res[content-length] - :response-time ms'),
